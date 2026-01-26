acf259d9678a759b8b9495ba324e21477337166b
TS01651-Lan-VueJS <lannnbts01651@gmail.com>
2026-01-26 21:05:15 +0700

    nộp bài lab5

---
+++ lab5/src/App.vue
@@ -0,0 +1,55 @@
+<!-- <script setup>
+import Bai1 from './components/bai1.vue';
+import Bai2 from './components/bai2.vue';
+
+
+</script>
+
+<template>
+  <Bai1/>
+  <Bai2/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style> -->
+
+
+<template>
+  <div id="app">
+    <h1>Ứng dụng Blog nhỏ với Vue.js</h1>
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import CreatePost from "./components/CreatePost.vue";
+import PostList from "./components/PostList.vue";
+
+const posts = ref([]);
+
+function addPost(post) {
+  posts.value.push(post);
+}
+</script>
+
+<style>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
